bf771963532c0843dd2370b71a2a61b1bbf876fb
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-11-16 11:20:30 -0300

    feat: add login view

---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Styles from './spinner-styles.scss'
+
+type SpinnerProps = React.HTMLAttributes<HTMLElement>
+
+const Spinner: React.FC<SpinnerProps> = (props: SpinnerProps) => {
+  return (
+    <div {...props} className={[Styles.spinner, props.className].join(' ')}>
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  )
+}
+
+export default Spinner
