b4a086a242c15fd388a91a5f2a52cd4c39ab51d6
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-11-24 17:30:49 -0300

    test: ensure ValidationBuilder has a minLength helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,5 +1,6 @@
 import {
   EmailValidation,
+  MinLengthValidation,
   RequiredFieldValidation
 } from '@/validation/validators'
 import { ValidationBuilder as sut } from './validation-builder'
@@ -13,5 +14,10 @@ describe('ValidationBuilder', () => {
   test('Should return EmailValidation', () => {
     const validations = sut.field('any').email().build()
     expect(validations).toEqual([new EmailValidation('any')])
+  })
+
+  test('Should return MinLengthValidation', () => {
+    const validations = sut.field('any').min(5).build()
+    expect(validations).toEqual([new MinLengthValidation('any', 5)])
   })
 })
