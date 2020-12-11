d86803dfc412fd9f5f70b07a6ecca1ed80eee006
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-12-11 11:52:28 -0300

    test: ensure SignUp loads with correct state

---
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -0,0 +1,18 @@
+import faker from 'faker'
+import * as FormHelper from '../support/form-helper'
+import * as Http from '../support/login-mock'
+
+describe('SignUp', () => {
+  beforeEach(() => {
+    cy.visit('signup')
+  })
+
+  it('Should load with correct initial state', () => {
+    FormHelper.testInputStatus('email', 'Campo Obrigatório')
+    FormHelper.testInputStatus('name', 'Campo Obrigatório')
+    FormHelper.testInputStatus('password', 'Campo Obrigatório')
+    FormHelper.testInputStatus('passwordConfirmation', 'Campo Obrigatório')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+})
