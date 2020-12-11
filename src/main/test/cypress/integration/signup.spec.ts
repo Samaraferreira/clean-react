71da4d241ec441d5ed1212ca305ddab7cd3c7bc3
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-12-11 12:22:49 -0300

    test: ensure SignUp save accessToken on localStorage on success

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -74,4 +74,12 @@ describe('SignUp', () => {
     FormHelper.testMainError('Ocorreu um erro. Tente novamente mais tarde.')
     FormHelper.testUrl('/signup')
   })
+
+  it('Should save accessToken if valid credentials are provided', () => {
+    Http.mockOk()
+    simulateValidSubmit()
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+    FormHelper.testUrl('/')
+    FormHelper.testLocalStorageItem('accessToken')
+  })
 })
