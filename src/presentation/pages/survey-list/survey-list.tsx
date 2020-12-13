f68f4046a2aa173880a93002d9d7ea0b659938a5
Samara Ferreira <ferreirasamara.sf@gmail.com>
2020-12-12 21:15:27 -0300

    feat: ensure SurveyItem load survey data correctly

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -24,7 +24,7 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <ul data-testid="survey-list">
-          <SurveyItem />
+          {/* <SurveyItem /> */}
           <SurveyItemEmpty />
         </ul>
       </div>
